1c6f02e081e465a1741cf476e94792c5a76e170e
Yaroslav Tsybukh <yaroslav.tsybukh@gmail.com>
2023-08-31 17:10:20 +0300

    create AddPost block

--- src/components/AddPost.tsx
+++ src/components/AddPost.tsx
@@ -1,11 +1,55 @@
-import { Card } from 'antd'
+import React from 'react'
+import { Button, Form, Input, Space, FormInstance, Upload, UploadProps } from 'antd'
+import { UploadOutlined } from '@ant-design/icons'
+
+const props: UploadProps = {
+    action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+    onChange({ file, fileList }) {
+        if (file.status !== 'uploading') {
+            console.log(file, fileList)
+        }
+    },
+}
 
 export const AddPost = () => {
+    const [form] = Form.useForm()
     return (
-        <Card title='Default size card' extra={<a href='#'>More</a>} style={{ width: 300 }}>
-            <p>Card content</p>
-            <p>Card content</p>
-            <p>Card content</p>
-        </Card>
+        <Form form={form} name='validateOnly' layout='vertical' autoComplete='off'>
+            <Form.Item name='name' rules={[{ required: true, message: 'Fill this field , please !' }]}>
+                <Input placeholder='Что у вас нового?' />
+            </Form.Item>
+            <Form.Item>
+                <Space size={100}>
+                    <Upload {...props}>
+                        <Button icon={<UploadOutlined />}>Upload</Button>
+                    </Upload>
+                    <SubmitButton form={form} />
+                </Space>
+            </Form.Item>
+        </Form>
+    )
+}
+
+const SubmitButton = ({ form }: { form: FormInstance }) => {
+    const [submittable, setSubmittable] = React.useState(false)
+
+    // Watch all values
+    const values = Form.useWatch([], form)
+
+    React.useEffect(() => {
+        form.validateFields({ validateOnly: true }).then(
+            () => {
+                setSubmittable(true)
+            },
+            () => {
+                setSubmittable(false)
+            },
+        )
+    }, [values])
+
+    return (
+        <Button type='primary' htmlType='submit' disabled={!submittable}>
+            Отправить новую запись
+        </Button>
     )
 }
