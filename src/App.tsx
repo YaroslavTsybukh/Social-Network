ef51713df889a6dd4ccdff6479aa352b44b79573
Yaroslav Tsybukh <yaroslav.tsybukh@gmail.com>
2023-09-08 12:47:03 +0300

    markup for auth and register pages

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,16 @@ import { Messages } from './pages/messages/Messages.tsx'
 import { Message } from './pages/message/Message.tsx'
 import { Friends } from './pages/friends/Friends.tsx'
 import { Profile } from './pages/profile/Profile.tsx'
+import { Auth } from './pages/auth/Auth.tsx'
+import { Register } from './pages/register/Register.tsx'
 
 const App = () => {
     return (
         <BrowserRouter>
             <Routes>
+                <Route path='/login' element={<Auth />} />
+                <Route path='/register' element={<Register />} />
+
                 <Route path='/' element={<Home />} />
                 {['/profile', '/friend/:friendId'].map((path, i) => (
                     <Route key={i} path={path} element={<Profile />} />
--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom'
 
 export const Header = () => {
     const [current, setCurrent] = useState('home')
+    const auth = false
     const onSearch = (value: string) => console.log(value)
     const onClick: MenuProps['onClick'] = (e) => {
         console.log('click ', e)
@@ -28,16 +29,27 @@ export const Header = () => {
             <Link to='/' style={{ display: 'inline-flex' }}>
                 <TrademarkCircleTwoTone style={{ fontSize: '45px' }} />
             </Link>
-            <nav>
-                <Menu mode='horizontal' selectedKeys={[current]} onClick={onClick} className='menu' items={menuItems} />
-            </nav>
-            <Input.Search
-                placeholder='input search text'
-                onSearch={onSearch}
-                enterButton
-                size='middle'
-                style={{ width: '500px' }}
-            />
+
+            {auth ? (
+                <>
+                    <nav>
+                        <Menu
+                            mode='horizontal'
+                            selectedKeys={[current]}
+                            onClick={onClick}
+                            className='menu'
+                            items={menuItems}
+                        />
+                    </nav>
+                    <Input.Search
+                        placeholder='input search text'
+                        onSearch={onSearch}
+                        enterButton
+                        size='middle'
+                        style={{ width: '500px' }}
+                    />
+                </>
+            ) : null}
         </Layout.Header>
     )
 }
