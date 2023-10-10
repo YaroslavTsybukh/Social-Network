837660b8a6fde4e6d23b96bd3c2af8e46d004d30
Yaroslav Tsybukh <yaroslav.tsybukh@gmail.com>
2023-10-10 18:10:44 +0300

    adding Spin during data is loading

--- src/components/profileInfo/ProfileInfoWrapper.tsx
+++ src/components/profileInfo/ProfileInfoWrapper.tsx
@@ -1,5 +1,6 @@
 import { FC, useEffect, useState } from 'react'
 import { doc, onSnapshot } from 'firebase/firestore'
+import { Spin } from 'antd'
 import { EmptyWithModal } from './EmptyWithModal.tsx'
 import { ProfileInfo } from './ProfileInfo.tsx'
 import { db } from '../../firebase.ts'
@@ -8,6 +9,7 @@ import { DocumentData } from 'firebase/firestore'
 
 export const ProfileInfoWrapper: FC<{ formName: string }> = ({ formName }) => {
     const [userData, setData] = useState<DocumentData | null>(null)
+    const [process, setProcess] = useState<string>('loading')
 
     useEffect(() => {
         const unsubscribe = onSnapshot(doc(db, 'userData', 'userInformation'), (doc) => {
@@ -16,12 +18,24 @@ export const ProfileInfoWrapper: FC<{ formName: string }> = ({ formName }) => {
             } else {
                 setData(null)
             }
+
+            setProcess('confirmed')
         })
 
         return () => unsubscribe()
     }, [])
 
+    if (process == 'loading') {
+        return <Spin />
+    }
+
     return (
-        <>{userData ? <ProfileInfo data={userData} formName={formName} /> : <EmptyWithModal formName={formName} />}</>
+        <>
+            {userData && process == 'confirmed' ? (
+                <ProfileInfo data={userData} formName={formName} />
+            ) : (
+                <EmptyWithModal formName={formName} />
+            )}
+        </>
     )
 }
